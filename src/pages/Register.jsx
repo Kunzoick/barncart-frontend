bf4d3bf3d9d74d43b411c30a8bde8fb1469845fe
Kunzoick <113906263+Kunzoick@users.noreply.github.com>
2026-06-03 19:04:17 -0400

    fix

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-\import { useState } from 'react'
+import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Eye, EyeOff } from 'lucide-react'
 import { registerUser } from '../api/auth'
